1b1d6c95efdfea943aaeeea519ea8ba0e8e35f21
Vijay <vjscjp@gmail.com>
2016-06-28 15:27:55 -0400

    Update code configuration based on env variable rather service name

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // Cisco Shipped sample three-tier application UI server
 // Change the assignments below if you change the API server service name
 // or port number from the default
-var API_SERVICE_NAME = "vote-api"
-var API_PORT = 8888
-var UI_PORT = 3000
-
-API_SERVICE_NAME = API_SERVICE_NAME.replace(/-/g, "_")
-
 var express = require('express');
 var http = require('http')
 var serveStatic = require("serve-static");
 
 var counter = 0
-
+var UI_PORT = 3000
+//Getting data from env HOST_VOTE_API
+var API_SERVICE_NAME = process.env.HOST_VOTE_API;
+//trim https:// or http:// 
+API_SERVICE_NAME = API_SERVICE_NAME.substring(API_SERVICE_NAME.indexOf("//")+2);
+console.log(API_SERVICE_NAME);
 var app = express()
 app.use(serveStatic(__dirname + "/."))
 
 // Endpoint 'count' - retrieve current count and store locally
 app.get('/count', function (req, res) {
+	
 	var options = {
-		host: API_SERVICE_NAME,
-		port: API_PORT,
-		path: "/data"
-	}
+	  host: API_SERVICE_NAME,
+	  port: 80,
+	  path: '/data'
+	};
 	http.get(options, function(getres) {	
 	  getres.on("data", function(chunk) {
 	    counter = JSON.parse(chunk).Data.Count
@@ -58,7 +58,7 @@ function updateCount(count) {
 	})
 	var request = new http.ClientRequest({
 		hostname: API_SERVICE_NAME,
-		port: API_PORT,
+		port: 80,
 		path: "/data",
 		method: "POST",
 		headers: {
